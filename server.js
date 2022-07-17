2e193dc9b0445e58e1a43190a3011fcda7d38297
o.semenuik <Sashasemenujk@gmail.com>
2022-07-17 16:27:35 +0300

    add server js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const path = require('path');
-const fs = require('fs');
 
 app.set('view engine', 'ejs');
 
